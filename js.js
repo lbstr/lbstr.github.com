e6410605e8a7103f8d31cf031dd6a6ac7f3ca58f
Jared Barnett <jared@jaredbarnett.com>
2012-08-25 20:34:55 -0600

    sprucing up

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ var MGR = function(c, w, h, fps){
         bodies = this.getBodies();
 
     this.interval = setInterval(function(){
-        ctx.fillStyle = "rgba(0, 0, 0, .2)";
+        ctx.fillStyle = "rgba(0, 0, 0, .26)";
         self.rect(ctx,0,0,w,h);
         for (var i = 0; i < bodies.length; i++) {
             bodies[i] = bodies[i].draw(ctx, w, h, bodies);
@@ -13,36 +13,12 @@ var MGR = function(c, w, h, fps){
     }, fps);
 };
 
-// (16777215).toString(16); // ffffff
-MGR.prototype.getUniqueColor = function(){
-    if(this.usedColors === undefined){
-        this.usedColors = {"#":0};
-    }
-    var used = this.usedColors;
-    var a = ["5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-    
-    do {
-        var s = "#";
-        var c = 6;
-        while(c--) {
-            s += a[Math.floor(Math.random() * a.length)];
-        }
-    } while(used[s] !== undefined);
-    
-    this.usedColors[s] = 0;
-
-    return s;
-};
-
 MGR.prototype.getBodies = function(){
     var bodies = [
-        new Body(400, 400, 0, 0, 10000, this.getUniqueColor()),
-        new Body(330, 400, 0, 4, 10, this.getUniqueColor()),
-        new Body(260, 400, 0, 3, 10, this.getUniqueColor()),
-        new Body(190, 400, 0, 2, 10, this.getUniqueColor()),
-        new Body(470, 400, 0, -4, 10, this.getUniqueColor()),
-        new Body(540, 400, 0, -3, 10, this.getUniqueColor()),
-        new Body(610, 400, 0, -2, 10, this.getUniqueColor())
+        new Body(8000, 300, 400, 0, -1.2),
+        new Body(8000, 700, 400, 0, 1.2),
+        new Body(500, 800, 400, 0, -2),
+        new Body(500, 200, 400, 0, 2)
     ];
     
     return bodies;
@@ -53,9 +29,9 @@ MGR.prototype.cleanupBodies = function(bodies){
         b;
     for (var i = 0; i < bodies.length; i++) {
         b = bodies[i];
-        if (b.children.length) {
-            a.push.apply(a,b);
-            b.children = [];
+        if (b.pieces.length) {
+            a.push.apply(a,b.pieces);
+            b.pieces = [];
         }
         if (b.m > 0) {
             a.push(b);
@@ -77,15 +53,15 @@ MGR.prototype.kill = function(){
 };
 
 
-var Body = function(ix, iy, ivx, ivy, m, color) {
-    this.x = ix;
-    this.y = iy;
-    this.vx = ivx;
-    this.vy = ivy;
+var Body = function(m, x0, y0, vx0, vy0) {
     this.m = m;
     this.r = this.getRfromM(m);
-    this.color = color;
-    this.children = [];
+    this.x = x0;
+    this.y = y0;
+    this.vx = vx0;
+    this.vy = vy0;
+    this.color = this.getUniqueColor();
+    this.pieces = [];
 };
 Body.prototype.draw = function(ctx, w, h, otherBodies) {
     ctx.fillStyle = this.color;
@@ -98,7 +74,21 @@ Body.prototype.draw = function(ctx, w, h, otherBodies) {
 Body.prototype.getRfromM = function(m){
     return Math.ceil(.4 + m/300);
 };
-
+// (16777215).toString(16); // ffffff
+Body.prototype.getUniqueColor = function(){
+    if(this.usedColors === undefined){
+        this.usedColors = {};
+    }
+    var used = this.usedColors;
+    var n;
+    while(1) {
+        n = Math.floor(Math.random() * 16777216);
+        if(used[n] === undefined) { break; }
+    }
+    used[n] = 0;
+    this.usedColors = used;
+    return "#" + n.toString(16);
+};
 Body.prototype.update = function(ctx, w, h, otherBodies) {
     var x0 = this.x,
         y0 = this.y,
@@ -110,7 +100,6 @@ Body.prototype.update = function(ctx, w, h, otherBodies) {
         b = otherBodies[i];
         if (b.color !== this.color) {
             f = this.F(b);
-            //this.G(ctx, b);
             Fx += f.x;
             Fy += f.y;
         }
@@ -144,36 +133,39 @@ Body.prototype.update = function(ctx, w, h, otherBodies) {
 Body.prototype.F = function(body){
     var x0 = this.x,
         y0 = this.y,
+        m = this.m,
         bx = body.x,
         by = body.y,
         bm = body.m,
         rSquared = Math.pow(bx-x0,2) + Math.pow(by-y0,2),
         r = Math.sqrt(rSquared),
         G = .1, //6.674 * Math.pow(10, -11),
-        F = G * this.m * bm / rSquared,
+        F = G * m * bm / rSquared,
         Fx = F * (bx - x0) / r,
         Fy = F * (by - y0) / r;
     
     // Collision
     if (r <= body.r + this.r) {
-        
+        /*
+        var d = (m - bm) / (m + bm),
+            vx = d * this.vx,
+            vy = d * this.vy;
+        if (x0 > bx) {
+            this.vx = -vx > 0 ? -vx : vx;
+        }
+        else {
+            this.vx = -vx > 0 ? vx : -vx;
+        }
+        if (y0 > by) {
+            this.vy = -vy > 0 ? -vy : vy;
+        }
+        else {
+            this.vy = -vy > 0 ? vy : -vy;
+        }
+        */
     }
     
     return {x: Fx, y: Fy};
-};
-
-Body.prototype.G = function(ctx, body){
-    var x0 = this.x,
-        y0 = this.y,
-        bx = body.x,
-        by = body.y;
-    
-    ctx.beginPath();
-    ctx.moveTo(x0, y0);
-    ctx.lineTo(bx, by);
-    ctx.closePath();
-    ctx.strokeStyle = "#a0c6ff";
-    ctx.stroke();
 };
 
 Body.prototype.consume = function(ctx, body){
@@ -188,8 +180,8 @@ Body.prototype.circle = function(ctx,x,y,r) {
 };
 
 $(document).ready(function(){
-	var w = window.innerWidth || document.body.clientWidth,
-        h = window.innerHeight || document.body.clientHeight;
+	var w = -5 + (window.innerWidth || document.body.clientWidth),
+        h = -5 + (window.innerHeight || document.body.clientHeight);
     
     $('#canvas-wrap').append('<canvas id="c" width="'+w+'" height="'+h+'"></canvas>');
     
